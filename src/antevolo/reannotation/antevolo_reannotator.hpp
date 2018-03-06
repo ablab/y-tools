44dbb3b08292b8bee4fbf607712f6c75a66558af
Andrey Slabodkin <sunisgood94@gmail.com>
2018-03-07 02:32:22 +0300

    Reannotator : WIP

--- src/antevolo/reannotation/antevolo_reannotator.hpp
+++ src/antevolo/reannotation/antevolo_reannotator.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-// #include <>
+#include <antevolo_config.hpp>
+#include <vj_class_processors/v_class_processor.hpp>
+#include <annotation_utils/annotated_clone_set.hpp>
 
 class AntevoloReannotator {
 
